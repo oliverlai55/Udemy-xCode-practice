b9ffa965605140398909ec8be11384c17c043224
oliverlai55 <oliverlai55@gmail.com>
2015-11-22 16:02:54 -0500

    Practice xCode with Udemy

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Wei lun Lai on 11/22/15.
+//  Copyright © 2015 Appfish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello World");
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
